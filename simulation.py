376bb1bb647695acf700437224b6250467f6ec6f
Kai N.B. McPheeters <kmcpheeters@fordham.edu>
2021-01-10 16:19:58 -0800

    Renamed bots list to starting_bots list

--- simulation.py
+++ simulation.py
@@ -1,14 +1,14 @@
 from bots import Detective, Grudger, Cooperator, Cheater, Copycat, Copykitten, Random
 from simulator import create_bot, count_bots, run_generation, new_generation
 
-bots = []
-bots+= create_bot(Detective,5)
-bots+= create_bot(Grudger,5)
-bots+= create_bot(Cooperator,5)
-bots+= create_bot(Cheater,10)
-bots+= create_bot(Copycat,5)
-bots+= create_bot(Copykitten,5)
-bots+= create_bot(Random,0)
+starting_bots = []
+starting_bots+= create_bot(Detective,5)
+starting_bots+= create_bot(Grudger,5)
+starting_bots+= create_bot(Cooperator,5)
+starting_bots+= create_bot(Cheater,10)
+starting_bots+= create_bot(Copycat,5)
+starting_bots+= create_bot(Copykitten,5)
+starting_bots+= create_bot(Random,0)
 
 for generation in range(0,10):
     run_generation(bots,rounds=10)
